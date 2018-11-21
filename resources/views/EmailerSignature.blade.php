1dd1df1dd6227edfd138fee4fdecf48c6c663790
Evans Tlhako <evans.tlhako@moreismore.global>
2018-11-21 13:42:04 +0200

    Source File

---
+++ resources/views/EmailerSignature.blade.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="utf-8">
+<title>Option 2</title>
+
+<!--[if mso]>
+ 
+<style>
+    span, td, table, div {
+      font-family: Arial, serif !important;
+    }
+</style>
+ 
+<![endif]-->
+<style type="text/css">
+a:link {
+	color: #000000;
+	text-decoration: none;
+}
+a:visited {
+	color: #000000;
+	text-decoration: none;
+}
+a:hover {
+	color: #000000;
+	text-decoration: none;
+}
+a:active {
+	color: #000000;
+	text-decoration: none;
+}
+</style>
+</head>
+
+<body style="font-family: Helvetica; font-size: 12px;">
+<table width="585" border="0" cellspacing="0" cellpadding="0">
+  <tbody>
+    <tr>
+      <td height="50" colspan="3" bgcolor="#000000" style="font-size: 12px;line-height: 19px;color: #fff; padding-bottom: 0px;padding-top: 3px; padding-left: 15px;  border-bottom-width:3px; border-bottom-color:#000000; border-bottom-style: solid;"><strong >PETER VAN JAARSVELD</strong> <br >
+        <span style="font-size: 12px; color: #FFFFFF; font-weight: normal;">Executive Director</span></td>
+      <td colspan="3" align="left" style="font-size: 26px; color: #000000; border-bottom-width:3px; border-bottom-color:#000000; border-bottom-style: solid; padding-left:0px; font-weight: bold;"><img src="images/logo.gif" width="317" height="45" alt="    Oliver + MORE"/></td>
+    </tr>
+    <tr>
+      <td colspan="1">&nbsp;</td>
+      <td height="5px" colspan="3" rowspan="1" valign="middle">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="10" width="14" rowspan="3" valign="middle">&nbsp;</td>
+      <td width="228" height="20"><a href="mailto:peter@moreismore.global" style="color: #000000; text-decoration: none;" title="Email">peter@moreismore.global</a></td>
+      <td width="25" height="20"></td>
+      <td width="33">&nbsp;</td>
+      <td width="285"><a href="tel:+27112349100" style="color: #000000; text-decoration: none;" title="Telephone">Tel: +27 (0) 11 234 9100</a></td>
+    </tr>
+    <tr>
+      <td><a href="https://moreismore.global" style="color: #000000; text-decoration: none; padding-bottom: 0px;" title="Website" target="_blank"> www.moreismore.global </a></td>
+      <td height="20"></td>
+      <td height="20">&nbsp;</td>
+      <td height="20"></td>
+    </tr>
+
+  </tbody>
+</table>
+</body>
+</html>
